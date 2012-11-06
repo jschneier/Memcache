81922f44b9d344ef1e7f1524c30d4812ab363a81
Josh Schneier <josh.schneier@gmail.com>
2012-11-06 13:05:44 -0500

    abstract out socket init

--- server.c
+++ server.c
@@ -23,7 +23,6 @@ typedef struct block {
 
 void *thread(void *vargp);
 int add_block(int, char *, char *);
-int init_socket(int *);
 
 static block *database[DBSIZE];
 
@@ -94,27 +93,4 @@ void *thread(void *vargp) {
         //if parse();
     }
     return NULL;
-}
-
-int init_socket(int *sock_fd) {
-
-    struct addrinfo hints, *info;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    if (getaddrinfo(NULL, PORT, &hints, &info) != 0)
-        return -1;
-
-    *sock_fd = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
-
-    if (*sock_fd == -1)
-        return -1;
-    if (bind(*sock_fd, info->ai_addr, info->ai_addrlen) == -1)
-        return -1;
-    if (listen(*sock_fd, BACKLOG) == -1)
-        return -1;
-    
-    return 0;
 }
